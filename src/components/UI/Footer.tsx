bccb988b2a32a0bdef24b67ea93fe99a511a1b3f
YarynaPelekh <yaryna.pelekh@hebronsoft.com>
2022-06-21 14:05:32 +0300

    added Header&Footer

---
+++ src/components/UI/Footer.tsx
@@ -0,0 +1,11 @@
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <header className={classes.footer}>
+      <p></p>
+    </header>
+  );
+};
+
+export default Footer;
---
+++ src/components/UI/Header.tsx
@@ -0,0 +1,11 @@
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <p>Notes Manager</p>
+    </header>
+  );
+};
+
+export default Header;
